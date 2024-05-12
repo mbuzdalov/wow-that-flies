2e1e75af97f644285139f867bfb96d4f418f959a
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-05-12 02:59:37 +0100

    Update the player demo.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/Main.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/Main.scala
@@ -1,14 +1,14 @@
 package com.github.mbuzdalov.wtf
 
 import java.awt.image.BufferedImage
-import java.net.Socket
+import java.io.FileInputStream
 
 import scala.util.Using
 
 object Main:
   private val usageStr =
     """Usage:
-      |  Main --input-port <port>
+      |  Main --input <pipe>
       |       --width <video width>
       |       --height <video height>
       |       --frame-rate <video frame rate>
@@ -21,49 +21,64 @@ object Main:
   extension (map: Map[String, String])
     private def getOrPrint(key: String): String = map.getOrElse(key, usage(s"No $key specified"))
 
-  private def flush(port: Int, width: Int, height: Int, fps: Double, target: FrameConsumer): Unit =
-    val buf = new Array[Byte](width * height * 3)
+  private def flush(pipe: String, width: Int, height: Int, fps: Double, target: FrameConsumer): Unit =
+    val buf = new Array[Byte](math.min(width * height * 3, 32768))
     val img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
 
-    Using.resource(new Socket("localhost", port)): socket =>
-      Using.resource(socket.getInputStream): ins =>
-        var frameNo = -1L
-        var x, y, pixI = 0
-        var bufSize = 0
-        var pixel = 0
-        while
-          bufSize = ins.read(buf)
-          bufSize > 0
-        do
-          var i = 0
-          while i < bufSize do
-            pixel |= (buf(i) & 0xff) << (8 * pixI)
-            i += 1
-            pixI += 1
-            if pixI == 3 then
-              pixI = 0
-              img.setRGB(x, y, pixel)
-              pixel = 0
-              x += 1
-              if x == width then
-                x = 0
-                y += 1
-                if y == height then
-                  y = 0
-                  frameNo += 1
-                  println(s"submitting frame $frameNo")
-                  target.consume(img, frameNo / fps, frameNo)
-        end while
+    Using.resource(new FileInputStream(pipe)): ins =>
+      var frameNo = -1L
+      var x, y, pixI = 0
+      var bufSize = 0
+      var pixel = 0
+      while
+        bufSize = ins.read(buf)
+        bufSize > 0
+      do
+        var i = 0
+        while i < bufSize do
+          pixel |= (buf(i) & 0xff) << (8 * pixI)
+          i += 1
+          pixI += 1
+          if pixI == 3 then
+            pixI = 0
+            img.setRGB(x, y, pixel)
+            pixel = 0
+            x += 1
+            if x == width then
+              x = 0
+              y += 1
+              if y == height then
+                y = 0
+                frameNo += 1
+                target.consume(img, frameNo / fps, frameNo)
+      end while
   end flush
+
+  private def getAutoArmedTime(reader: LogReader): Double =
+    val eventValues = reader.connect[Numbers.UInt8]("EV", "Id")
+    val eventTimes = reader.connect[Numbers.UInt64]("EV", "TimeUS")
+
+    val autoArmedEventIndex = (0 until eventValues.size).find(i => eventValues.get(i).toInt == 15).get
+    eventTimes.get(autoArmedEventIndex).toDouble * 1e-6
 
   def main(args: Array[String]): Unit =
     val map = args.grouped(2).map(a => a(0) -> a(1)).toMap
-    val port = map.getOrPrint("--input").toInt
+    val pipe = map.getOrPrint("--input")
     val width = map.getOrPrint("--width").toInt
     val height = map.getOrPrint("--height").toInt
     val fps = map.getOrPrint("--frame-rate").toDouble
+    val armTime = map.getOrPrint("--arm-time").toDouble
 
-    val fc: FrameConsumer = new Player
-    flush(port, width, height, fps, fc)
+    val log = map.getOrPrint("--log")
+    val reader = Using.resource(new FileInputStream(log))(is => new LogReader(new ByteStorage(is)))
+
+    val autoArmedEventTime = getAutoArmedTime(reader)
+    val logTimeOffset = autoArmedEventTime - armTime + 0.15
+
+    println(s"Auto-armed event found at time $autoArmedEventTime, offset = $logTimeOffset")
+
+    val sticks = new Sticks(reader, logTimeOffset, 301, 340, 680, 210)
+    val player = new Player
+    flush(pipe, width, height, fps, FrameConsumer.compose(sticks, player))
   end main
 end Main
