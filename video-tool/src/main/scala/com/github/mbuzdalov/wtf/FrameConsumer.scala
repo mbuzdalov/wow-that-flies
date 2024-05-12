b36bedb9c667ffc4276f8edbc4f557045cc4b126
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-05-12 02:58:59 +0100

    Support aggregation of frame consumers.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/FrameConsumer.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/FrameConsumer.scala
@@ -4,4 +4,8 @@ import java.awt.image.BufferedImage
 
 trait FrameConsumer:
   def consume(img: BufferedImage, time: Double, frameNo: Long): Unit
-  
+
+object FrameConsumer:
+  def compose(consumers: FrameConsumer*): FrameConsumer = 
+    (img: BufferedImage, time: Double, frameNo: Long) => consumers.foreach(_.consume(img, time, frameNo))
+    
