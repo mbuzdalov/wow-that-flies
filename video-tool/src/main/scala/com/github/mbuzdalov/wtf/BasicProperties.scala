c2e32f8bf9649995fccc0185c4ccb5632ec29e58
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-05-16 00:27:26 +0100

    Move property parsing to lazy values of BasicProperties itself, remove the usage thing for a while.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/BasicProperties.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/BasicProperties.scala
@@ -4,13 +4,18 @@ import java.io.FileInputStream
 
 import scala.util.Using
 
-case class BasicProperties(input: String, output: String, log: String,
-                           width: Int, height: Int, fps: Double, armTime: Double):
+class BasicProperties(args: Array[String]):
+  private val map = args.tail.grouped(2).map(a => a(0) -> a(1)).toMap
+  private lazy val input: String = map("--input")
+  private lazy val output: String = map("--output")
+  private lazy val log: String = map("--log")
+  lazy val width: Int = map("--width").toInt
+  lazy val height: Int = map("--height").toInt
+  private lazy val fps: Double = map("--frame-rate").toDouble
+  lazy val armTime: Double = map("--arm-time").toDouble
+
   def createLogReader: LogReader = Using.resource(new FileInputStream(log))(is => new LogReader(new ByteStorage(is)))
   def run(consumers: GraphicsConsumer*): Unit =
-    val last = output match
-      case "player" => new Player
-      case _ => new VideoWriter(output)
-
-    IOUtils.feedFileToConsumer(input, width, height, fps, 
-      FrameConsumer.compose(FrameConsumer(GraphicsConsumer.compose(consumers*)), last))
+    val destination = if output == "player" then new Player else new VideoWriter(output)
+    IOUtils.feedFileToConsumer(input, width, height, fps,
+      FrameConsumer.compose(FrameConsumer(GraphicsConsumer.compose(consumers*)), destination))
--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/Main.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/Main.scala
@@ -5,38 +5,8 @@ import scala.language.implicitConversions
 import com.github.mbuzdalov.wtf.videos.*
 
 object Main:
-  private val usageStr =
-    """Usage:
-      |  Main <video-id>
-      |       --input <filename>
-      |       --width <video width>
-      |       --height <video height>
-      |       --frame-rate <video frame rate>
-      |       --arm-time <arm time in video>
-      |       --log <ArduPilot binary log>
-      |       --output <filename | player>
-      |  <video-id> is the internal video id to choose the particular script to run
-      |  <filename> can be - for standard input or output
-      |""".stripMargin
-
-  private def usage(extraMsg: String): Nothing =
-    System.err.println(if extraMsg.nonEmpty then s"$extraMsg\n$usageStr" else usageStr)
-    sys.exit(1)
-
-  extension (map: Map[String, String])
-    private def getOrPrint(key: String): String = map.getOrElse(key, usage(s"No $key specified"))
-
   def main(args: Array[String]): Unit =
-    val map = args.tail.grouped(2).map(a => a(0) -> a(1)).toMap
-    val props = BasicProperties(
-      input = map.getOrPrint("--input"),
-      output = map.getOrPrint("--output"),
-      log = map.getOrPrint("--log"),
-      width = map.getOrPrint("--width").toInt,
-      height = map.getOrPrint("--height").toInt,
-      fps = map.getOrPrint("--frame-rate").toDouble,
-      armTime = map.getOrPrint("--arm-time").toDouble,
-    )
+    val props = BasicProperties(args.tail)
 
     args(0) match
       case "2024-03-28" => V_2024_03_28(props)
