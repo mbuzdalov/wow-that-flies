3826134f5d810ff673a840d301ecd7a37cd994fc
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-05-27 01:51:00 +0100

    Add a widget that blinks a circle around something to attract attention.

---
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/widgets/BlinkingCircle.scala
@@ -0,0 +1,29 @@
+package com.github.mbuzdalov.wtf.widgets
+
+import java.awt.{Color, Graphics2D}
+import java.awt.image.BufferedImage
+
+import com.github.mbuzdalov.wtf.GraphicsConsumer
+
+class BlinkingCircle(start: Double, period: Double, finish: Double,
+                     x: Double, y: Double, rInner: Double, rOuter: Double,
+                     color: Color) extends GraphicsConsumer:
+
+  override def consume(img: BufferedImage, g: Graphics2D, time: Double, frameNo: Long): Unit =
+    val remainder = (time - start) % period
+    if time >= start && time <= finish && (remainder < period / 2 - 1e-9 || remainder > 1 - 1e-9) then
+      val xMin = math.max(0, math.floor(x - rOuter).toInt)
+      val xMax = math.min(img.getWidth - 1, math.ceil(x + rOuter).toInt)
+      val yMin = math.max(0, math.floor(y - rOuter).toInt)
+      val yMax = math.min(img.getHeight - 1, math.ceil(y + rOuter).toInt)
+      if xMin <= xMax && yMin <= yMax then
+        var yy = yMin
+        while yy <= yMax do
+          var xx = xMin
+          while xx <= xMax do
+            val dist = math.hypot(xx - x, yy - y)
+            if dist >= rInner && dist <= rOuter then
+              img.setRGB(xx, yy, color.getRGB)
+            xx += 1
+          yy += 1
+    end if
