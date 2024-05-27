2d7a18293ca0778c8447933c869fb5a685bc99d4
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-05-27 05:08:03 +0100

    Implement a widget for the last log message.

---
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/widgets/LastLogMessage.scala
@@ -0,0 +1,27 @@
+package com.github.mbuzdalov.wtf.widgets
+
+import java.awt.Graphics2D
+import java.awt.image.BufferedImage
+
+import scala.compiletime.uninitialized
+
+import com.github.mbuzdalov.wtf.{GraphicsConsumer, LogReader}
+
+class LastLogMessage(logReader: LogReader, logTimeOffset: Double,
+                     colorFont: TextMessage.ColorFont, x: Float, y: Float,
+                     timeOn: TextMessage.Alpha, timeOff: TextMessage.Alpha) extends GraphicsConsumer:
+  private val msgConnectT = logReader.timingConnect("MSG")
+  private val msgConnectM = logReader.connect[String]("MSG", "Message")
+  private var message: TextMessage = uninitialized
+
+  override def consume(img: BufferedImage, g: Graphics2D, time: Double, frameNo: Long): Unit =
+    if time >= timeOn.time && time <= timeOff.time then
+      val t0 = time + logTimeOffset
+      val idx = msgConnectT.indexForTime(t0)
+      if msgConnectT.get(idx) < t0 - 2 then
+        message = null
+      else if message == null || msgConnectM.get(idx) != message.text then
+        message = TextMessage(msgConnectM.get(idx), colorFont, x, y,
+              TextMessage.HorizontalAlignment.Left, TextMessage.VerticalAlignment.Bottom, timeOn, timeOff)
+      end if
+      if message != null then message.consume(img, g, time, frameNo)
