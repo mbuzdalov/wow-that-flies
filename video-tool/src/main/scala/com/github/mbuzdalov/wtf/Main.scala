1a764f74d3d7441aeae65d92426739cd12a764a8
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-07-21 14:44:53 +0100

    In Main, replace video pattern match with a reflective call.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/Main.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/Main.scala
@@ -2,46 +2,11 @@ package com.github.mbuzdalov.wtf
 
 import scala.language.implicitConversions
 
-import com.github.mbuzdalov.wtf.videos.*
-
 object Main:
   def main(args: Array[String]): Unit =
     val props = BasicProperties(args.tail)
-
-    args(0) match
-      case "2024-03-28" => V_2024_03_28(props)
-      case "2024-03-29-p1" => V_2024_03_29_p1(props)
-      case "2024-03-29-p2" => V_2024_03_29_p2(props)
-      case "2024-03-29-p3" => V_2024_03_29_p3(props)
-      case "2024-04-08-p1" => V_2024_04_08_p1(props)
-      case "2024-04-08-p2" => V_2024_04_08_p2(props)
-      case "2024-04-08-p3" => V_2024_04_08_p3(props)
-      case "2024-04-08-p4" => V_2024_04_08_p4(props)
-      case "2024-04-09-p1" => V_2024_04_09_p1(props)
-      case "2024-04-09-p2" => V_2024_04_09_p2(props)
-      case "2024-04-14-p1" => V_2024_04_14_p1(props)
-      case "2024-04-14-p2" => V_2024_04_14_p2(props)
-      case "2024-04-14-p3" => V_2024_04_14_p3(props)
-      case "2024-04-15-p1" => V_2024_04_15_p1(props)
-      case "2024-04-15-p2" => V_2024_04_15_p2(props)
-      case "2024-04-15-p3" => V_2024_04_15_p3(props)
-      case "2024-04-15-p4" => V_2024_04_15_p4(props)
-      case "2024-04-20-p1" => V_2024_04_20_p1(props)
-      case "2024-04-20-p2" => V_2024_04_20_p2(props)
-      case "2024-04-20-p3" => V_2024_04_20_p3(props)
-      case "2024-04-21-p1" => V_2024_04_21_p1(props)
-      case "2024-04-21-p2" => V_2024_04_21_p2(props)
-      case "2024-04-24" => V_2024_04_24(props)
-      case "2024-05-09-p1" => V_2024_05_09_p1(props)
-      case "2024-05-09-p2" => V_2024_05_09_p2(props)
-      case "2024-05-09-p3" => V_2024_05_09_p3(props)
-      case "2024-05-09-p4" => V_2024_05_09_p4(props)
-      case "2024-05-09-p5" => V_2024_05_09_p5(props)
-      case "2024-05-10-p1" => V_2024_05_10_p1(props)
-      case "2024-05-10-p2" => V_2024_05_10_p2(props)
-      case "2024-06-09-p1" => V_2024_06_09_p1(props)
-      case "2024-06-09-p2" => V_2024_06_09_p2(props)
-      case "2024-06-23-p1" => V_2024_06_23_p1(props)
-      case "2024-06-23-p2" => V_2024_06_23_p2(props)
+    val className = s"com.github.mbuzdalov.wtf.videos.V_${args(0).replace('-', '_')}"
+    val method = Class.forName(className).getDeclaredMethod("apply", classOf[BasicProperties])
+    method.invoke(null, props)
   end main
 end Main
