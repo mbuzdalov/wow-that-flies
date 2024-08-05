e97a1e9f5de6ae3318aca2158639585823141e73
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-08-05 06:48:14 +0100

    Add rude support for non-centered scaling in ScaleRotateCropBack.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/widgets/ScaleRotateCropBack.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/widgets/ScaleRotateCropBack.scala
@@ -7,7 +7,9 @@ import scala.compiletime.uninitialized
 
 import com.github.mbuzdalov.wtf.GraphicsConsumer
 
-class ScaleRotateCropBack(scale: Double => Double, rotate: Double => Double) extends GraphicsConsumer:
+class ScaleRotateCropBack(scale: Double => Double, rotate: Double => Double, 
+                          scaleCenterX: (Double, Double) => Double = (w, t) => w / 2, 
+                          scaleCenterY: (Double, Double) => Double = (h, t) => h / 2) extends GraphicsConsumer:
   private var tempImage: BufferedImage = uninitialized
   override def consume(img: BufferedImage, g: Graphics2D, time: Double, frameNo: Long): Unit =
     if tempImage == null then tempImage = new BufferedImage(img.getWidth, img.getHeight, img.getType)
@@ -17,7 +19,7 @@ class ScaleRotateCropBack(scale: Double => Double, rotate: Double => Double) ext
     val origTransform = g.getTransform
     g.rotate(angle, img.getWidth / 2.0, img.getHeight / 2.0)
     g.scale(scl, scl)
-    g.translate(-(scl - 1) * img.getWidth / 2, -(scl - 1) * img.getHeight / 2)
+    g.translate(-(scl - 1) * scaleCenterX(img.getWidth, time), -(scl - 1) * scaleCenterY(img.getHeight, time))
     g.drawImage(tempImage, 0, 0, null)
     g.setTransform(origTransform)
 
