c0c20bcc0f3dd924043085e62b612488bfe9925f
Maxim Buzdalov <mbuzdalov@gmail.com>
2025-03-29 15:53:11 +0000

    Remove unused plots in 2025-03-2x part 1.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/videos/V_2025_03_2x_p1.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/videos/V_2025_03_2x_p1.scala
@@ -24,21 +24,6 @@ object V_2025_03_2x_p1:
     val sticks = new Sticks(reader, logTimeOffset, stickSize,
       stickW - stickSize - stickGap, stickW + stickGap, stickGap)
 
-    val rpGap = 11 * width / 1280
-    val rpWidth = width / 6
-    val rpHeight = height / 7
-    val fontSize = 16f * width / 1280
-    val rpBackground = new Color(255, 255, 255, 120)
-
-    val rollPlot = Plot.createRollPitchPlot(reader, logTimeOffset, "Roll", -1, 20,
-      rpGap, rpGap, rpWidth, rpHeight, fontSize, rpBackground, 2, 3)
-    val pitchPlot = Plot.createRollPitchPlot(reader, logTimeOffset, "Pitch", -1, 30,
-      rpGap * 2 + rpWidth, rpGap, rpWidth, rpHeight, fontSize, rpBackground, 2, 5)
-    val yawPlot = Plot.createYawPlot(reader, logTimeOffset, 3, 4,
-      width - 2 * rpGap - 2 * rpWidth, rpGap, rpWidth, rpHeight, fontSize, rpBackground, 2, 3)
-    val flapPlot = Plot.createXFlapPlot(reader, logTimeOffset,
-      width - rpGap - rpWidth, rpGap, rpWidth, rpHeight, fontSize, rpBackground, 2, 3)
-
     val msgFontSize = 33f * width / 1280
     val msgStep = msgFontSize * 1.5f
     val msgCF = TextMessage.ColorFont(msgFontSize, new Color(40, 10, 40))
@@ -47,7 +32,7 @@ object V_2025_03_2x_p1:
     val blinkOuterR = width * 0.05
 
     val allGraphics = GraphicsConsumer.compose(
-      sticks, //rollPlot, pitchPlot, yawPlot, flapPlot,
+      sticks,
 
       BottomBlanket(0.35f, new Color(240, 240, 240, 200), 1.0, 1.4, 9.5, 9.9),
       TextMessage("Morning, March 24, 2025.",
