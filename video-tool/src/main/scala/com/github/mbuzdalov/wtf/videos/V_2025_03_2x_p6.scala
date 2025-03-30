f2bf10872b8b6021589e5a4a1f36e8fb9500377b
Maxim Buzdalov <mbuzdalov@gmail.com>
2025-03-30 05:16:40 +0100

    Parts 6 and 7 of the March 2x video.

---
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/videos/V_2025_03_2x_p6.scala
@@ -0,0 +1,59 @@
+package com.github.mbuzdalov.wtf.videos
+
+import java.awt.Color
+
+import com.github.mbuzdalov.wtf.widgets.*
+import com.github.mbuzdalov.wtf.widgets.TextMessage.{HorizontalAlignment as HA, VerticalAlignment as VA}
+import com.github.mbuzdalov.wtf.{BasicProperties, GraphicsConsumer}
+
+object V_2025_03_2x_p6:
+  def apply(props: BasicProperties): Unit =
+    val width = props.width
+    val height = props.height
+    val reader = props.createLogReader()
+
+    val autoArmedTimes = reader.autoArmedTimes
+    require(autoArmedTimes.size == 7) // 6 real ones, 1 did not actually arm
+    val logTimeOffset = autoArmedTimes(5) - props.armTime
+
+    val stickMiddle = (width * 0.42).toInt
+    val stickSize = 201 * width / 1920 / 2
+    val stickGap = 21 * width / 1920 / 2
+    val sticks = new Sticks(reader, logTimeOffset, stickSize,
+      stickMiddle - stickSize - stickGap, stickMiddle + stickGap, stickGap)
+
+    val msgFontSize = 33f * width / 1280
+    val msgStep = msgFontSize * 1.5f
+    val msgCF = TextMessage.ColorFont(msgFontSize, new Color(40, 10, 40))
+
+    val allGraphics = GraphicsConsumer.compose(
+      sticks.enabledBetween(8, 37),
+      FlightMode(reader, logTimeOffset, msgCF,
+        stickMiddle + stickSize + 2 * stickGap, stickGap + stickSize / 2, HA.Left, VA.Bottom).enabledBetween(8, 37),
+      VerticalSpeed(reader, logTimeOffset, msgCF,
+        stickMiddle + stickSize + 2 * stickGap, stickGap + stickSize / 2, HA.Left, VA.Top).enabledBetween(8, 37),
+      NEDWidget(reader, logTimeOffset,
+        minN = 6.5, maxN = 26.5,
+        minE = -2, maxE = 18,
+        minAlt = -2, maxAlt = 26,
+        visMinX = 0.15, visMaxX = 0.85, visMinY = 0.15, visMaxY = 0.95,
+        yawOverride = Some(_ => 170.0)).enabledBetween(13, 30),
+
+      TopBlanket(0.16, new Color(240, 240, 240, 200), 1.0, 1.4, 6.5, 6.9),
+      TextMessage("Fifth test.",
+        msgCF, width * 0.5, 0.6 * msgStep, HA.Center, VA.Center).enabledBetween(1.5, 6.5),
+      TextMessage("An attempt to ascend even higher.",
+        msgCF, width * 0.5, 1.6 * msgStep, HA.Center, VA.Center).enabledBetween(1.5, 6.5),
+
+      BottomBlanket(0.16, new Color(240, 240, 240, 200), 36.0, 36.4, 39.5, 39.9),
+      TextMessage("The first hiccup during today's testing!",
+        msgCF, width * 0.5, height - 1.6 * msgStep, HA.Center, VA.Center).enabledBetween(36.5, 39.5),
+      TextMessage("Fortunately, nothing got broken.",
+        msgCF, width * 0.5, height - 0.6 * msgStep, HA.Center, VA.Center).enabledBetween(36.5, 39.5),
+
+      Fade(timeOn = 0.5, timeOff = 0),
+      Fade(timeOn = 40.5, timeOff = 41.0),
+    )
+
+    props.runVideo(allGraphics)
+  end apply
---
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/videos/V_2025_03_2x_p7.scala
@@ -0,0 +1,69 @@
+package com.github.mbuzdalov.wtf.videos
+
+import java.awt.Color
+
+import com.github.mbuzdalov.wtf.widgets.*
+import com.github.mbuzdalov.wtf.widgets.TextMessage.{HorizontalAlignment as HA, VerticalAlignment as VA}
+import com.github.mbuzdalov.wtf.{BasicProperties, GraphicsConsumer}
+
+object V_2025_03_2x_p7:
+  def apply(props: BasicProperties): Unit =
+    val width = props.width
+    val height = props.height
+    val reader = props.createLogReader()
+
+    val autoArmedTimes = reader.autoArmedTimes
+    require(autoArmedTimes.size == 7) // 6 real ones, 1 did not actually arm
+    val logTimeOffset = autoArmedTimes(6) - props.armTime
+
+    val stickMiddle = (width * 0.42).toInt
+    val stickSize = 201 * width / 1920 / 2
+    val stickGap = 21 * width / 1920 / 2
+    val sticks = new Sticks(reader, logTimeOffset, stickSize,
+      stickMiddle - stickSize - stickGap, stickMiddle + stickGap, stickGap)
+
+    val msgFontSize = 33f * width / 1280
+    val msgStep = msgFontSize * 1.5f
+    val msgCF = TextMessage.ColorFont(msgFontSize, new Color(40, 10, 40))
+
+    val allGraphics = GraphicsConsumer.compose(
+      sticks.enabledBetween(18, 37),
+      FlightMode(reader, logTimeOffset, msgCF,
+        stickMiddle + stickSize + 2 * stickGap, stickGap + stickSize / 2, HA.Left, VA.Bottom).enabledBetween(18, 37),
+      VerticalSpeed(reader, logTimeOffset, msgCF,
+        stickMiddle + stickSize + 2 * stickGap, stickGap + stickSize / 2, HA.Left, VA.Top).enabledBetween(18, 37),
+      NEDWidget(reader, logTimeOffset,
+        minN = 6.5, maxN = 6.5 + 20.0 * 10 / 14,
+        minE = -2, maxE = -2 + 20.0 * 10 / 14,
+        minAlt = -2, maxAlt = 18,
+        visMinX = 0.2, visMaxX = 0.8, visMinY = 0.3, visMaxY = 0.9,
+        yawOverride = Some(_ => 170.0)).enabledBetween(23, 37.5),
+
+      TopBlanket(0.35, new Color(240, 240, 240, 200), 1.0, 1.4, 11.5, 11.9),
+      TextMessage("So, we had large slow oscillations during landing,",
+        msgCF, width * 0.5, 0.5 * msgStep, HA.Center, VA.Center).enabledBetween(1.5, 11.5),
+      TextMessage("both during the initial and the final phases, but not in Stabilize.",
+        msgCF, width * 0.5, 1.5 * msgStep, HA.Center, VA.Center).enabledBetween(1.5, 11.5),
+      TextMessage("Despite that, SN1 did not fly away, and got no visible damage.",
+        msgCF, width * 0.5, 3 * msgStep, HA.Center, VA.Center).enabledBetween(4.5, 11.5),
+      TextMessage("This did look like a tuning issue, but not in low-level PID tuning.",
+        msgCF, width * 0.5, 4.5 * msgStep, HA.Center, VA.Center).enabledBetween(7, 11.5),
+
+      TopBlanket(0.16, new Color(240, 240, 240, 200), 13.0, 13.4, 17.5, 17.9),
+      TextMessage("Sixth test.",
+        msgCF, width * 0.5, 0.6 * msgStep, HA.Center, VA.Center).enabledBetween(13.5, 17.5),
+      TextMessage("Was this just a fluke, or is this reproducible?",
+        msgCF, width * 0.5, 1.6 * msgStep, HA.Center, VA.Center).enabledBetween(13.5, 17.5),
+
+      BottomBlanket(0.16, new Color(240, 240, 240, 200), 38.0, 38.4, 41.5, 41.9),
+      TextMessage("Oooooops...",
+        msgCF, width * 0.5, height - 1.6 * msgStep, HA.Center, VA.Center).enabledBetween(38.5, 41.5),
+      TextMessage("Surely this requires a mishap investigation...",
+        msgCF, width * 0.5, height - 0.6 * msgStep, HA.Center, VA.Center).enabledBetween(38.5, 41.5),
+
+      Fade(timeOn = 0.5, timeOff = 0),
+      Fade(timeOn = 42.5, timeOff = 43.0),
+    )
+
+    props.runVideo(allGraphics)
+  end apply
