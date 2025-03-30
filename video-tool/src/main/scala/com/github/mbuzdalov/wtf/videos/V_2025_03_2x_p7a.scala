3396dae23988fc4a3b165e6b3fd469bdafee5ab2
Maxim Buzdalov <mbuzdalov@gmail.com>
2025-03-30 15:37:01 +0100

    Add the "a" counterparts for parts 6 and 7.

---
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/videos/V_2025_03_2x_p7a.scala
@@ -0,0 +1,28 @@
+package com.github.mbuzdalov.wtf.videos
+
+import java.awt.Color
+
+import com.github.mbuzdalov.wtf.widgets.*
+import com.github.mbuzdalov.wtf.widgets.TextMessage.{HorizontalAlignment as HA, VerticalAlignment as VA}
+import com.github.mbuzdalov.wtf.{BasicProperties, GraphicsConsumer}
+
+object V_2025_03_2x_p7a:
+  def apply(props: BasicProperties): Unit =
+    val width = props.width
+    val height = props.height
+
+    val msgFontSize = 33f * width / 1280
+    val msgStep = msgFontSize * 1.5f
+    val msgCF = TextMessage.ColorFont(msgFontSize, new Color(40, 10, 40))
+
+    val allGraphics = GraphicsConsumer.compose(
+      BottomBlanket(0.1f, new Color(240, 240, 240, 200), 1.0, 1.4, 4.5, 4.9),
+      TextMessage("6th test, the FPV feed (not used to fly)",
+        msgCF, width * 0.5, height * 0.95, HA.Center, VA.Center).enabledBetween(1.5, 4.5),
+
+      Fade(timeOn = 0.5, timeOff = 0),
+      Fade(timeOn = 27.5, timeOff = 28.0),
+    )
+
+    props.runVideo(allGraphics)
+  end apply
