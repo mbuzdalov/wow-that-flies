6b91ff483ff26633e8f46708eb1a7c085289b7d8
Maxim Buzdalov <mbuzdalov@gmail.com>
2025-06-04 17:53:46 +0100

    Now draw rectangles via 2D shapes, not via fillRect.

--- video-tool/src/main/scala/com/github/mbuzdalov/wtf/widgets/ColoredRectangle.scala
+++ video-tool/src/main/scala/com/github/mbuzdalov/wtf/widgets/ColoredRectangle.scala
@@ -1,5 +1,6 @@
 package com.github.mbuzdalov.wtf.widgets
 
+import java.awt.geom.Rectangle2D
 import java.awt.image.BufferedImage
 import java.awt.{Color, Graphics2D}
 
@@ -10,5 +11,5 @@ class ColoredRectangle(x: Double, y: Double, w: Double, h: Double, backgroundCol
   assert(h >= 0, "Height is negative, nothing will be visible")
   override def consume(img: BufferedImage, g: Graphics2D, time: Double, frameNo: Long): Unit =
     g.setColor(backgroundColor)
-    g.fillRect((x * img.getWidth).toInt, (y * img.getHeight).toInt, (w * img.getWidth).toInt, (h * img.getHeight).toInt)
+    g.fill(new Rectangle2D.Double(x * img.getWidth, y * img.getHeight, w * img.getWidth, h * img.getHeight))
   end consume
